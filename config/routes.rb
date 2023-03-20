e96b891254ba158788aeb9dd23f31db5488947fb
maqiie <99504010+maqiie@users.noreply.github.com>
2023-03-20 03:12:06 +0300

    completed the lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  
   patch "/birds/:id/like", to: "birds#increment_likes"
+resources :birds, only: [:index, :show, :create, :update, :destroy]
 end
